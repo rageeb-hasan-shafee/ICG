d136419fa6b2e8ab419b41cde1df67fbb28b7e14
rageeb-hasan-shafee <2105175@ugrad.cse.buet.ac.bd>
2025-07-22 23:57:18 +0600

    New Test Case Added

---
+++ antlr4-skeletons/cpp/input/loop2.c
@@ -0,0 +1,23 @@
+int main(){
+    int a,b,c, d, i,J,p;
+    b=0;
+	c=0;
+
+    for ( i = 10; i > 3; i--)
+    {
+        // for (J = 0; J < 4; J++)
+        // {
+            // for ( p = 0; p < 5; p++)
+            // {
+                c++;
+                println(c);
+        //     }
+            
+        // }
+        
+    }
+    
+    // println(a);
+    println(b);
+    println(c);
+}
